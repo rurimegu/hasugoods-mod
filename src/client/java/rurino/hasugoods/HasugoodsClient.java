4847848a38d1c93fc83e2010a832fb3f4ace5236
KCFindstr <792491320@qq.com>
2025-02-17 18:19:22 -0800

    Install fabric

---
+++ src/client/java/rurino/hasugoods/HasugoodsClient.java
@@ -0,0 +1,10 @@
+package rurino.hasugoods;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HasugoodsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
