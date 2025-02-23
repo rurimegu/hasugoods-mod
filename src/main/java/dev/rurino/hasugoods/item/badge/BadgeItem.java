534a2bf70c6b051fa7a436c8d1e104ef8a7632e5
KCFindstr <792491320@qq.com>
2025-02-23 00:05:59 -0800

    Add secret badges

--- src/main/java/dev/rurino/hasugoods/item/badge/BadgeItem.java
+++ src/main/java/dev/rurino/hasugoods/item/badge/BadgeItem.java
@@ -11,6 +11,7 @@ import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.DeathProtectionComponent;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.consume.ClearAllEffectsConsumeEffect;
 import net.minecraft.item.consume.ConsumeEffect;
 import net.minecraft.loot.LootPool;
@@ -24,7 +25,7 @@ import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Rarity;
 
 public class BadgeItem extends OshiItem {
-
+  // #region Static fields
   public static final DeathProtectionComponent HASU_BADGE_DEATH_PROTECTION = new DeathProtectionComponent(
       List.<ConsumeEffect>of(new ClearAllEffectsConsumeEffect(), new ToutoshiEffectsConsumeEffect()));
   public static final TagKey<Item> BADGE_TAG = TagKey.of(RegistryKeys.ITEM, Hasugoods.id("badges"));
@@ -32,10 +33,16 @@ public class BadgeItem extends OshiItem {
   public static final RegistryKey<Item> RURINO_BADGE_KEY = RegistryKey.of(RegistryKeys.ITEM,
       Hasugoods.id(OshiItem.RURINO_KEY + "_badge"));
   public static final Item RURINO_BADGE = registerBadge(RURINO_BADGE_KEY, OshiItem.RURINO_KEY);
+  public static final RegistryKey<Item> RURINO_SECRET_BADGE_KEY = RegistryKey.of(RegistryKeys.ITEM,
+      Hasugoods.id(OshiItem.RURINO_KEY + "_secret_badge"));
+  public static final Item RURINO_SECRET_BADGE = registerSecretBadge(RURINO_SECRET_BADGE_KEY, OshiItem.RURINO_KEY);
 
   public static final RegistryKey<Item> MEGUMI_BADGE_KEY = RegistryKey.of(RegistryKeys.ITEM,
       Hasugoods.id(OshiItem.MEGUMI_KEY + "_badge"));
   public static final Item MEGUMI_BADGE = registerBadge(MEGUMI_BADGE_KEY, OshiItem.MEGUMI_KEY);
+  public static final RegistryKey<Item> MEGUMI_SECRET_BADGE_KEY = RegistryKey.of(RegistryKeys.ITEM,
+      Hasugoods.id(OshiItem.MEGUMI_KEY + "_secret_badge"));
+  public static final Item MEGUMI_SECRET_BADGE = registerSecretBadge(MEGUMI_SECRET_BADGE_KEY, OshiItem.MEGUMI_KEY);
 
   public static final RegistryKey<Item> UNOPENED_BADGE_KEY = RegistryKey.of(RegistryKeys.ITEM,
       Hasugoods.id("unopened_badge"));
@@ -43,17 +50,21 @@ public class BadgeItem extends OshiItem {
       new UnopenedBadge(new Item.Settings().maxCount(16).rarity(Rarity.COMMON).registryKey(UNOPENED_BADGE_KEY)),
       UNOPENED_BADGE_KEY);
 
-  private static Item registerBadge(RegistryKey<Item> key, String oshiKey) {
+  private static Item registerBadge(RegistryKey<Item> key, String oshiKey, boolean isSecret) {
     Item item = ModItems.register(
-        new BadgeItem(new Item.Settings().maxCount(16).rarity(Rarity.COMMON)
+        new BadgeItem(new Settings().maxCount(16).rarity(isSecret ? Rarity.UNCOMMON : Rarity.COMMON)
             .component(DataComponentTypes.DEATH_PROTECTION, HASU_BADGE_DEATH_PROTECTION)
-            .registryKey(key), oshiKey),
+            .registryKey(key), oshiKey, isSecret),
         key);
     return item;
   }
 
-  public BadgeItem(Settings settings, String oshiName) {
-    super(settings, oshiName);
+  private static Item registerBadge(RegistryKey<Item> key, String oshiKey) {
+    return registerBadge(key, oshiKey, false);
+  }
+
+  private static Item registerSecretBadge(RegistryKey<Item> key, String oshiKey) {
+    return registerBadge(key, oshiKey, true);
   }
 
   public static void initialize() {
@@ -68,12 +79,29 @@ public class BadgeItem extends OshiItem {
       if (source.isBuiltin() && key.getValue().getPath().startsWith("chests/")) {
         // Give 0~3 badges, or 20 badges if really lucky.
         LootPool.Builder poolBuilder = LootPool.builder()
-            .with(ItemEntry.builder(UNOPENED_BADGE).weight(98).quality(0)
+            .with(ItemEntry.builder(UNOPENED_BADGE).weight(95).quality(0)
                 .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0, 3))))
-            .with(ItemEntry.builder(UNOPENED_BADGE).weight(2).quality(0)
-                .apply(SetCountLootFunction.builder(ConstantLootNumberProvider.create(20))));
+            .with(ItemEntry.builder(UNOPENED_BADGE).weight(5).quality(0)
+                .apply(SetCountLootFunction.builder(ConstantLootNumberProvider.create(16))));
         tableBuilder.pool(poolBuilder);
       }
     });
+  }
+  // #endregion Static fields
+
+  private final boolean isSecret;
+
+  public BadgeItem(Settings settings, String oshiName, boolean isSecret) {
+    super(settings, oshiName);
+    this.isSecret = isSecret;
+  }
+
+  public boolean isSecret() {
+    return isSecret;
+  }
+
+  @Override
+  public boolean hasGlint(ItemStack stack) {
+    return isSecret() ? true : super.hasGlint(stack);
   }
 }
