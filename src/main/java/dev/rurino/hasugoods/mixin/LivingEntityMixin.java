2b774be59dbf1fefcd1ac0dfda5124bd65ba5da4
KCFindstr <792491320@qq.com>
2025-02-25 01:24:41 -0800

    Fix build issues

--- src/main/java/dev/rurino/hasugoods/mixin/LivingEntityMixin.java
+++ src/main/java/dev/rurino/hasugoods/mixin/LivingEntityMixin.java
@@ -1,18 +1,18 @@
 package dev.rurino.hasugoods.mixin;
 
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import dev.rurino.hasugoods.Hasugoods;
+
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
 	// @Inject(at = @At("HEAD"), method =
-	// "hasStatusEffect(Lnet/minecraft/registry/entry/RegistryEntry)Z", cancellable
-	// = true)
+	// "hasStatusEffect(Lnet/minecraft/registry/entry/RegistryEntry)Z")
 	// private void hasStatusEffect(CallbackInfo info) {
-	// LivingEntity entity = (LivingEntity) (Object) this;
+	// Hasugoods.LOGGER.info("hasStatusEffect mixin");
 	// }
 }
