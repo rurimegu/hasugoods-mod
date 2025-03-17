c6024ef648d9e8b07fe809ffddfc83a18bbe4628
KCFindstr <792491320@qq.com>
2025-03-17 03:00:53 -0700

    Remove unnecessary space around icon particles

--- src/main/java/dev/rurino/hasugoods/util/Timer.java
+++ src/main/java/dev/rurino/hasugoods/util/Timer.java
@@ -1,7 +1,27 @@
 package dev.rurino.hasugoods.util;
 
+import java.util.LinkedList;
+
+import dev.rurino.hasugoods.Hasugoods;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.util.Pair;
+import net.minecraft.world.World;
+
 public class Timer {
-  private static int CANCELED = -1;
+  private static final int CANCELED = -1;
+
+  private static LinkedList<Pair<World, Timer>> worldTickTimers = new LinkedList<>();
+
+  static {
+    ServerTickEvents.START_WORLD_TICK.register((w) -> {
+      worldTickTimers.removeIf(pair -> {
+        if (pair.getLeft() == w) {
+          return pair.getRight().tick();
+        }
+        return false;
+      });
+    });
+  }
 
   private int tick;
   private int repeat;
@@ -21,6 +41,15 @@ public class Timer {
 
   public static Timer loop(int tick, Runnable action) {
     return new Timer(tick, action, Integer.MAX_VALUE);
+  }
+
+  public Timer attachToServerTick(World world) {
+    if (world.isClient) {
+      Hasugoods.LOGGER.warn("Timer cannot attach to server tick in client world, ignored.");
+      return this;
+    }
+    worldTickTimers.add(new Pair<>(world, this));
+    return this;
   }
 
   public int remaining() {
